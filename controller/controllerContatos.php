cdd21916b6217b979a622dacd376f670f3fd6d12
LEONARDO BOMFIM ANTUNES <21276366@CFP127ALUNO.LOCAL>
2022-03-04 17:01:42 -0300

    04/03

---
+++ controller/controllerContatos.php
@@ -0,0 +1,42 @@
+<?php
+ /////////////////////////////////////////////////////
+//                                                  //
+// Arquivo responsavel pela manipulação de dados de //
+// contato                                          //
+// Obs: este arquivo fará a ponte entre a view e a  //
+//   model                                          //
+//                                                  //
+// Autor  :  Leonardo Antunes                       //
+// Data   :  04/03/22                               //
+// Versão :  1.0                                    //
+/////////////////////////////////////////////////////
+
+/**
+ * Inserir novos contatos atravez dos dados recebidos pela view
+ */
+function inserirContato(){
+
+}
+
+/**
+ * Atualizar os contatos atravez dos dados recebidos pela view
+ */
+function atualizarContato(){
+
+}
+
+/**
+ * Excluir contatos atravez dos dados recebidos pela view
+ */
+function excluirContato(){
+
+}
+
+/**
+ * Solicita os dados de contato da model e encaminha para a view
+ */
+function listarContato(){
+
+}
+
+?>
